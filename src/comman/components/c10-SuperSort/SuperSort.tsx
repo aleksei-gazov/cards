104f87a1bf3868387d8202b28d3d734d6ea7c226
aleksei-gazov <lekha.gazov@yandex.ru>
2023-04-04 22:13:11 +0300

    create universal-components

---
+++ src/comman/components/c10-SuperSort/SuperSort.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+// добавить в проект иконки и импортировать
+// const downIcon = '[\\/]'
+// const upIcon = '[/\\]'
+// const noneIcon = '[--]'
+
+const downIcon = <ExpandMoreIcon/>
+const upIcon = <ExpandLessIcon/>
+const noneIcon = <HorizontalRuleIcon/>
+
+export type SuperSortPropsType = {
+    id?: string
+    sort: string
+    value: string
+    onChange: (newSort: string) => void
+}
+
+export const pureChange = (sort: string, down: string, up: string) => {
+    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
+    return sort === down
+        ? up
+        : sort == up
+            ? ''
+            : down
+}
+
+const SuperSort: React.FC<SuperSortPropsType> = (
+    {
+        sort, value, onChange, id = 'hw15',
+    }
+) => {
+    const up = '0' + value
+    const down = '1' + value
+
+    const onChangeCallback = () => {
+        onChange(pureChange(sort, down, up))
+    }
+
+    const icon = sort === down
+        ? downIcon
+        : sort === up
+            ? upIcon
+            : noneIcon
+
+    return (
+        <span
+            id={id + '-sort-' + value}
+            onClick={onChangeCallback}
+        >
+            {/*сделать иконку*/}
+            {/*<img*/}
+            {/*    id={id + '-icon-' + sort}*/}
+            {/*    src={icon}*/}
+            {/*/>*/}
+            {icon}
+        </span>
+    )
+}
+
+export default SuperSort
