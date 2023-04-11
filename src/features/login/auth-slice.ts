d67058cff37a79ad4921a1f3cbf9b2beb7fcfe11
Maksim_Kiselev <laurkurt@gmail.com>
2023-04-11 20:58:21 +0300

    refactoring and adding eslint/pretier/stylelint

--- src/features/login/auth-slice.ts
+++ src/features/login/auth-slice.ts
@@ -1,41 +1,16 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {registrationAPI} from "../registration/registration-api";
-import {AppRootState} from "../../app/store";
-
-const registerThunk = createAsyncThunk<{ isRegistered:boolean } , { email: string, password: string }, { rejectValue:string, state:AppRootState }>
-("auth/register", async (arg, thunkAPI)=>{
-    const {rejectWithValue} = thunkAPI
-    try{
-        await registrationAPI.register(arg.email, arg.password)
-
-        return {isRegistered:true}
-    }
-    catch (e:any){
-        return rejectWithValue(e)
-    }
-})
+import { createSlice } from '@reduxjs/toolkit'
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState:  {
-        isLoggedIn: false,
-        isInitialized: false,
-        isRegistered: false
-    },
-    reducers: {
-
-    },
-    extraReducers:builder => {
-        builder
-            .addCase(registerThunk.fulfilled, (state, action) => {
-                state.isRegistered = action.payload.isRegistered
-            })
-    }
+  name: 'auth',
+  initialState: {
+    isLoggedIn: false,
+    isInitialized: false,
+  },
+  reducers: {},
 })
 
 export const authReducer = authSlice.reducer
 export const {
-    // setIsLoggedInAC,
-    // setInitialStateAC,
-    } = authSlice.actions
-export const authThunks = {registerThunk}
+  // setIsLoggedInAC,
+  // setInitialStateAC,
+} = authSlice.actions
--- src/features/registration/registration-api.ts
+++ src/features/registration/registration-api.ts
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const registrationAPI = {
-    register(email:string, password:string){
-        return axios.post<{error?:string}>("http://localhost:7542/2.0/auth/register",
-            {email, password},
-            {withCredentials:true,}
-        )
-    }
-
+  register(email: string, password: string) {
+    return axios.post<{ error?: string }>(
+      'http://localhost:7542/2.0/auth/register',
+      { email, password },
+      { withCredentials: true }
+    )
+  },
 }
